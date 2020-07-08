b0307405eb4fd67a973fea4ed43fd04447ed6d20
karlmanalo <karlmanalo@comcast.net>
2020-07-08 09:25:59 -0600

    Turns in U1S1M2 Assignment

--- src/14_cal.py
+++ src/14_cal.py
@@ -32,8 +32,19 @@ import calendar
 from datetime import datetime
 import argparse
 
-parser = argparse.ArgumentParser(description="Prints the calendar for a given month.")
-parser.add_argument("month", metavar="MO", type=int, help="Month of desired calendar.")
-parser.add_argument("year", metavar="YR", type=int, help="Year of desired calendar")
+current_time = datetime.now()
 
-args = parser.parse_args()
+def print_calendar(month=current_time.month, year=current_time.year):
+  # Check if sys.argv[1] and sys.argv[2] exist to avoid errors?
+  month = sys.argv[1]
+  year = sys.argv[2]
+  return calendar.month(year, month)
+
+# How to call this function since default arguments exist? Excluding default args from
+# function may be easier, but isn't in the spirit of the assignment
+
+# parser = argparse.ArgumentParser(description="Prints the calendar for a given month.")
+# parser.add_argument("month", metavar="MO", type=int, help="Month of desired calendar.")
+# parser.add_argument("year", metavar="YR", type=int, help="Year of desired calendar")
+
+# args = parser.parse_args()
